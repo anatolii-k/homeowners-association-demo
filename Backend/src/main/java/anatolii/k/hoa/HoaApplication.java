47553b52e7e9c6ebc450e93a6e4aee6f34f9221d
anatolii-k <anatoly.zp@gmail.com>
2025-04-03 15:27:20 +0300

    Initial Commit

---
+++ Backend/src/main/java/anatolii/k/hoa/HoaApplication.java
@@ -0,0 +1,13 @@
+package anatolii.k.hoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaApplication.class, args);
+	}
+
+}
